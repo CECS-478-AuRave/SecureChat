64f2a555e3f01e7cb77875b45e156c773ea989bb
Aaron Turner <aaron@aaronthedev.com>
2016-10-03 13:56:09 -0700

    Generated the auth login page

---
+++ client/secureChatIonic/app/pages/auth-login/auth-login.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the AuthLoginPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/auth-login/auth-login.html',
+})
+export class AuthLoginPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
