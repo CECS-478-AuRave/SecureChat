f84f53404b1016c18f83bc52333f33217878b065
Aaron Turner <aaron@aaronthedev.com>
2016-10-05 11:31:00 -0700

    Added Https support for the app

--- backend/secure-chat-express/app.js
+++ backend/secure-chat-express/app.js
@@ -18,7 +18,9 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,9 +29,9 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -37,24 +39,43 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
+
+//https support
+//following: https://jaredreich.com/blog/how-to-create-nodejs-server/
+//and https://github.com/RnbWd/ssltest/blob/master/bin/www
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var privateKey = fs.readFileSync('ssl/privkey.pem');
+var certificate = fs.readFileSync('ssl/cert.pem');
+var CACertificate = fs.readFileSync('ssl/chain.pem');
+var credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: CACertificate
+};
+
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(4780);
 
 
 module.exports = app;
