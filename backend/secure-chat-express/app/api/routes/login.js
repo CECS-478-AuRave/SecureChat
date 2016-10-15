6ffc805ccd81458a18f25b5002930591521bebe0
kuminin <kumin.in@hotmail.com>
2016-10-15 16:14:36 -0700

    Facebook Routing is done :D. Hope it works :P

--- backend/secure-chat-express/app/api/routes/login.js
+++ backend/secure-chat-express/app/api/routes/login.js
@@ -1,11 +1,42 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
-
 module.exports = function(app, passport) {
-  app.get('/', )
+
+    // Route for main page where status response is 200.
+    app.get('/', isLoggedIn, function(req, res) {
+        res.status(200).end();
+    });
+
+    /// Route for login page where respond with status 401.
+    app.get('/login', function(req, res) {
+        res.status(401).end();
+    });
+
+    // Routes for facebook authentication
+    app.get('/auth/facebook', passport.authenticate('facebook'));
+
+    // Route for callback handling after facebook authentication
+    app.get('/auth/facebook/callback',
+        passport.authenticate(
+            'facebook', {
+                    successRedirect: '/',
+                    failureRedirect: '/login',
+            }
+        )
+    );
+
+    // Route for logging out, if the user is already logged in.
+    app.get('/logout', isLoggedIn, function(req, res) {
+        req.logout();
+        res.status(200).redirect('/login');
+    });
 };
+
+var isLoggedIn = function(req, res, next) {
+
+    // If the user is already authenticated then continue.
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    // Redirect to login page if the user isn't logged in
+    res.redirect('/login');
+}
