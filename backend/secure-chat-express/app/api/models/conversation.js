d3a3d7ba8ebf494061a0a50da6cba3684c1991ad
kuminin <kumin.in@hotmail.com>
2016-10-18 16:07:44 -0700

    added conversation schema

---
+++ backend/secure-chat-express/app/api/models/conversation.js
@@ -0,0 +1,18 @@
+'use strict';
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var messageSchema = new Schema({
+    message: {type: String, required: true, maxlength: 240},
+    from: {type: String, required: true},
+    date: {type: Date, default: Date.now}
+});
+
+var conversationSchema = new Schema({
+    conversation: {
+        id: {type: String, required: true}
+    },
+    message: [messageSchema];
+});
+
+mongoose.model('Conversation', conversationSchema);
