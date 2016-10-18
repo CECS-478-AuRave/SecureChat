e27efdb99ecb6d6fc94dfb17b624e252f37b3af7
kuminin <kumin.in@hotmail.com>
2016-10-18 13:28:27 -0700

    Added comments and added route to check if a user session exists.

--- backend/secure-chat-express/app/api/routes/routes.js
+++ backend/secure-chat-express/app/api/routes/routes.js
@@ -9,13 +9,22 @@ module.exports = function(app, passport) {
     //     res.end('{login:success}');
     // });
 
+    // Route for checking if the user is already logged in the session.
+    app.get('/api/v1/login', isLoggedIn, function(req, res) {
+        res.status(200).json({'Login': 'Successful'});
+    });
+
+    // Route for logging in.
     app.post('/api/v1/login',
         passport.authenticate(['facebook-token']),
         function(req, res) {
+            // Check if the user was authenticated
             if (req.user) {
+                // Respond with status 200 and JSON
                 res.status(200).json({'login': 'yes',
                                       'user': req.user});
             } else {
+                // Respond with Unauthorized access.
                 res.status(401).json({'login': 'no',
                                       'user': 'Hello Darkness My Old Friend'});
             }
@@ -39,16 +48,17 @@ module.exports = function(app, passport) {
     //                 failureRedirect: '/login',
     //         }
     //     ));
+
     // Route for logging out, if the user is already logged in.
-    // app.get('/logout', isLoggedIn, function(req, res) {
-    //     req.logout();
-    //     res.redirect('/login');
-    // });
+    app.get('/logout', isLoggedIn, function(req, res) {
+        req.logout();
+    });
 
     // Route for getting user information based on their facebook id.
     app.get('/api/v1/user/id/:id',
         passport.authenticate(['facebook-token']),
         function(req, res) {
+            // Check if the user was authenticated
             if (req.user) {
                 var facebookID = req.params.id;
 
@@ -70,6 +80,7 @@ module.exports = function(app, passport) {
     app.get('/api/v1/user/email/:email',
         passport.authenticate(['facebook-token']),
         function(req, res) {
+            // Check if the user was authenticated
             if (req.user) {
                 var email = req.params.email;
 
@@ -79,6 +90,7 @@ module.exports = function(app, passport) {
                     return;
                 }
 
+                // Find a user based on their email and return that user.
                 User.findOne({'email' : email}, findUser);
             } else {
                 res.status(401).json({'error': 'Access Not Authorized.'});
@@ -88,23 +100,27 @@ module.exports = function(app, passport) {
 
 var findUser = function(err, user) {
     if (!user) {
-        // if the user wasn't found.
+        // if the user wasn't found respond with status 404
+        // and the information stating User was not found.
         res.status(404).json({'error': 'User was not found.'});
     } else if (err) {
-        // if there was an error.
+        // if there was an error respond with status 404
+        // and the err information.
         res.status(404).json(err);
     } else {
         // if there wasn't an error and the user was found.
+        // respond with status 200 and user's information
         res.status(200).json(user);
     }
 };
-// var isLoggedIn = function(req, res, next) {
-//
-//     // If the user is already authenticated then continue.
-//     if (req.isAuthenticated()) {
-//         return next();
-//     }
-//
-//     // Redirect to login page if the user isn't logged in
-//     res.redirect('/login');
-// }
+
+var isLoggedIn = function(req, res, next) {
+
+    // If the user is already authenticated then continue.
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    // Redirect to login page if the user isn't logged in
+    res.status(401).json({'error' : 'login required'});
+}
