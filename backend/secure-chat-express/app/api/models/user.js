7f3090e03b52347662fcaf85138e506063302c17
kuminin <kumin.in@hotmail.com>
2016-10-19 13:20:56 -0700

    Changed friends and pendingFriends to hold Strings instead of id object

--- backend/secure-chat-express/app/api/models/user.js
+++ backend/secure-chat-express/app/api/models/user.js
@@ -21,8 +21,8 @@ var userSchema = new Schema({
     publicKey: {
         type: String
     },
-    friends: [id: {type: String}],
-    pendingFriends: [id: {type: String}]
+    friends: [String],
+    pendingFriends: [String]
 });
 
 mongoose.model('User', userSchema);
