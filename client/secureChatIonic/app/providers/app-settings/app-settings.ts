ba13b6cda7fef86b2d12d807c9f1b526b35655de
Aaron Turner <aaron@aaronthedev.com>
2016-10-20 19:53:03 -0700

    Started refactoring, and Created observables for Facebook Sdk
    As the title states, more refactoring needs to be done however

--- client/secureChatIonic/app/providers/app-settings/app-settings.ts
+++ client/secureChatIonic/app/providers/app-settings/app-settings.ts
@@ -4,17 +4,27 @@ export class AppSettings {
   //How to ignore changes on this file: http://stackoverflow.com/a/17410119
   //DO NOT UPLOAD ACTUAL KEYS TO GITHUB
 
-  //Declare our keys
-  static testing = 'testing test';
-
   //Fake key for now, to get the dialog working
   //Can be stored on the client: http://stackoverflow.com/questions/6709883/facebook-app-security-what-if-someone-uses-my-appid
-  static facebookAppId = '2365441780952680';
+  static facebookAppId = '2345671234542689';
 
   //The name of the json object stored on the device
+  /*
+  schema
+  {
+        user: {
+            name:""
+            email:""
+            etc....
+        }
+        keys: {
+            public: 'sdjlaksjda'
+            private: 'askjdklsjd'
+        },
+        access_token: 'token'
+    }
+  */
   static shushItemName = 'shushUser';
-
-
 
   //Add our server URL (Local testing not actual)
   //Must end with a slash
