e73e0428812f41937074e750705d11b109d753da
Aaron Turner <aaron@aaronthedev.com>
2016-10-20 19:06:43 -0700

    Got Message Requests working
    Need to do a major refactoring before moving forward

--- client/secureChatIonic/app/providers/app-messaging/app-messaging.ts
+++ client/secureChatIonic/app/providers/app-messaging/app-messaging.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 //Import our providers
 import { AppSettings } from '../../providers/app-settings/app-settings';
-import { AppNotify } from '../../providers/app-notify/app-notify';
 
 /*
   Generated class for the AppMessaging provider.
@@ -15,45 +14,19 @@ import { AppNotify } from '../../providers/app-notify/app-notify';
 @Injectable()
 export class AppMessaging {
 
-  constructor(private http: Http, private appSettings: AppSettings, private appNotify: AppNotify) { }
+  constructor(private http: Http, private appSettings: AppSettings) { }
 
   //Return all the conversations for a user
-  public getConversations(token) {
+  conversationRequest(token) {
 
-    //Start Loading
-    this.appNotify.startLoading('Getting Messages...');
-
-    //Our access_token header
-    let headers = {
+    //Our headers
+    let headers = new Headers({
       access_token: token
-    };
+    });
+    let options = new RequestOptions({ headers: headers });
 
     //Send the request with the payload to the server
-    var response = this.http.get(AppSettings.serverUrl + 'conversation', headers).map(res => res.json());
-
-    //Get a reference to this
-    let self = this;
-
-    response.subscribe(function(success) {
-      //Success!
-      console.log(success);
-
-      //Stop loading
-      return self.appNotify.stopLoading().then(function() {
-        return success;
-      });
-    }, function(error) {
-      //Error!
-
-      //Stop Loading
-      self.appNotify.stopLoading().then(function() {
-        //Pass to Error Handler
-        self.appNotify.handleError(error);
-      });
-
-    }, function() {
-      //Completed
-    })
+    return this.http.get(AppSettings.serverUrl + 'conversation', options).map(res => res.json());
 
 
   }
