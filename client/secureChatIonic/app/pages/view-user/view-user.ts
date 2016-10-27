c058f596eb1b2ed930e5acc0e346ca5887ac3189
Aaron Turner <aaron@aaronthedev.com>
2016-10-26 21:12:01 -0700

    FInished the view user page
    Without any logic. Still requires keys, and getting a user's friends

--- client/secureChatIonic/app/pages/view-user/view-user.ts
+++ client/secureChatIonic/app/pages/view-user/view-user.ts
@@ -20,9 +20,10 @@ export class ViewUserPage {
 
   constructor(private changeDetector: ChangeDetectorRef, private navCtrl: NavController, private navParams: NavParams) {
     if (!this.navParams.get('friend')) {
+
       //Initialize the page with the user
-      let user = JSON.parse(localStorage.getItem(AppSettings.shushItemName));
-      this.user = user.user;
+      let user = JSON.parse(localStorage.getItem(AppSettings.shushItemName)).user;
+      this.user = user;
       console.log(this.user);
 
       //Update the UI
@@ -30,6 +31,19 @@ export class ViewUserPage {
     }
 
     //Grab the friend
+  }
+
+  //Get the user type. 0 = not friend, 1 = friend, 2 = current user
+  getUserType() {
+    //Get our current user
+    let user = JSON.parse(localStorage.getItem(AppSettings.shushItemName)).user;
+    //If they are the user, return 2
+    if (this.user._id == user._id) return 2;
+
+    //Now check if it is a user or not
+    //If they are not the user, return 0. else return 1;
+    if (this.user.friends.indexOf(user._id) < 0) return 0;
+    else return 1;
   }
 
 }
