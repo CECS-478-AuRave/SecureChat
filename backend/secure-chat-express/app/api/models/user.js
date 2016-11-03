c48c0ccedf889b443813ae921fddef398484ed28
kuminin <kumin.in@hotmail.com>
2016-11-03 16:45:47 -0700

    Updated user to be searchable using mongoose-searchable

--- backend/secure-chat-express/app/api/models/user.js
+++ backend/secure-chat-express/app/api/models/user.js
@@ -1,7 +1,8 @@
 "use strict";
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
+    ObjectId = Schema.ObjectId,
+    searchable = require('mongoose-searchable');
 
 var userSchema = new Schema({
     name: {
@@ -25,4 +26,11 @@ var userSchema = new Schema({
     pendingFriends: [String]
 });
 
+// make all string fields searchable.
+userSchema.plugin(searchable, {
+  language: 'english',
+  fields: ['email', 'name']
+});
+
+// Compile userSchema into a mongoose model.
 mongoose.model('User', userSchema);
