9553adf28c6252a4050d9a4fd3f8952f6cebc5e5
Aaron Turner <aaron@aaronthedev.com>
2016-11-03 19:00:43 -0700

    Almost finished making friends
    As the title states. All that is left is making and responding to API calls!

--- client/secureChatIonic/app/pages/view-user/view-user.ts
+++ client/secureChatIonic/app/pages/view-user/view-user.ts
@@ -17,10 +17,27 @@ import { AppUsers} from '../../providers/app-users/app-users';
 })
 export class ViewUserPage {
 
+  //Map of what each user type means, and map to what request to make on the user
+  userTypeMap: any;
+
+  //The user that is being displayer
   user: any;
 
   constructor(private changeDetector: ChangeDetectorRef, private navCtrl: NavController, private navParams: NavParams, private appNotify: AppNotify, private appUsers: AppUsers) {
 
+    //Create our map of user types, and their requests
+    this.userTypeMap = {
+      currentUser: 0,
+      notFriend: 1,
+      pendingFriend: 2,
+      friend: 3,
+      requests: {
+        addFriend: 10,
+        acceptRequest: 11,
+        declineRequest: 12,
+        deleteFriend: 13
+      }
+    }
 
     //Get our user
     let user = JSON.parse(localStorage.getItem(AppSettings.shushItemName)).user;
@@ -34,6 +51,14 @@ export class ViewUserPage {
     //Check if we should show our user, return if it is
     if (!passedUser || passedUser._id == user._id) return;
 
+    //Get the user
+    this.getUser(passedUser);
+
+
+  }
+
+  //Function to get the user
+  getUser(passedUser) {
     //Start Loading
     this.appNotify.startLoading('Getting User...');
 
@@ -69,7 +94,15 @@ export class ViewUserPage {
     });
   }
 
-  //Get the user type. 0 = is the user, 1 = not friend, 2 = pending friend, 3 = friend
+  //Function that will make the needed request for the user
+  //E.g, add friend, remove friend etc...
+  //Generalized since they will all just toast the result, and update the UI
+  editUserStatus(requestType) {
+    //TODO: Write API Call
+    console.log(requestType);
+  }
+
+  //Get the user type, refers to the user type map in the constructor
   getUserType() {
 
     //CHeck if we currently have a user
@@ -77,15 +110,16 @@ export class ViewUserPage {
 
     //Get our current user
     let user = JSON.parse(localStorage.getItem(AppSettings.shushItemName)).user;
+
     //If they are the user, return
-    if (this.user._id == user._id) return 0;
+    if (this.user._id == user._id) return this.userTypeMap.currentUser;
 
     //Check if they are a friend, return 3
-    if (this.user.friends.indexOf(user._id) > 0) return 3;
+    if (this.user.friends.indexOf(user._id) > 0) return this.userTypeMap.friend;
     //Check if they are a pending friend
-    else if (this.user.pendingFriends.indexOf(user._id) > 0) return 2;
+    else if (this.user.pendingFriends.indexOf(user._id) > 0) return this.userTypeMap.pendingFriend;
     //Else they are not our friend
-    else return 1;
+    else return this.userTypeMap.notFriend;
   }
 
 }
